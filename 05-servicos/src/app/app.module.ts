9d9a8fe270cf1148a2f1dfcd61d8a0f007c71268
Mateus <mmorae26@ford.com>
2022-11-23 15:19:31 -0300

    injetando um servico em outro servico

--- 05-servicos/src/app/app.module.ts
+++ 05-servicos/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/curso.module';
+import { LogService } from './shared/log.service';
 
 
 
@@ -18,6 +19,7 @@ import { CursosModule } from './cursos/curso.module';
     CriarCursoModule,
     CursosModule
   ],
+  providers: [LogService],
   // providers: [CursosService],
   bootstrap: [AppComponent]
 })
