44fa93a2b4cd5191cfe969bc092f16dc6ded04e4
Mateus <mmorae26@ford.com>
2023-01-04 16:01:32 -0300

    Formulários reativos: Verificar validação dos campos com botão submit

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -47,15 +47,39 @@ export class DataFormComponent implements OnInit {
   onSubmit() {
     console.log(this.formulario)
 
-    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+    if (this.formulario.valid) {
+
+      this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
       .pipe(map(res => res))
       .subscribe(dados => {
         console.log(dados);
         // reseta o form
         // this.formulario.reset();
-        this.resetar();
+        // this.resetar();
       },
-      (error: any) => alert('erro'))
+      (error: any) => alert('erro')
+      );
+
+    }
+
+    else {
+      console.log('formulario invalido');
+      this.verificaValidacoesForm(this.formulario);
+    }
+  }
+
+  verificaValidacoesForm(formGroup: any) {
+
+    Object.keys(formGroup.controls).forEach(campo => {
+      console.log(campo);
+      const controle = formGroup.get(campo);
+      controle?.markAsDirty();
+      if (controle instanceof FormGroup) {
+        this.verificaValidacoesForm(controle)
+      }
+    })
+
+  
   }
 
   resetar() {
@@ -69,7 +93,7 @@ export class DataFormComponent implements OnInit {
   }
 
     verificaValidTouched(campo: string) {
-      return !this.formulario.get(campo)?.valid && !!this.formulario.get(campo)?.touched
+      return !this.formulario.get(campo)?.valid && (!!this.formulario.get(campo)?.touched || !!this.formulario.get(campo)?.dirty)
   }
 
   verificaEmailInvalido() {
